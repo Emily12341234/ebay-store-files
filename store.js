93add73867ad0363324a6ffac6a86e173ae77387
Abdullah Al Masud <amasud400@gmail.com>
2020-03-27 11:29:58 +0600

    Update store.js

--- store.js
+++ store.js
@@ -84,7 +84,7 @@ $f(document).ready(function(){
 $f(window).load(function(){
 	$f.ajax({	
 	method : "post",
-	url : "https://drive.google.com/uc?id=1gHJ6QNS4lpegWgJfvq4ztcrcoxYyskpz",
+	url : "https://masud1995.github.io/emilystorefiles/index.php",
 	async:false,
 	data:"newarrival=1",
 	success: function(result){
